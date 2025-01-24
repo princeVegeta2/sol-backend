0e606742f733c304da2e6d6ef0f2b789086eea42
princeVegeta2 <nellatorbeaufort@gmail.com>
2025-01-24 17:52:22 +0100

    EB Configuration Test 2 1/24/2025

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://prypto.netlify.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    allowedHeaders: 'Content-Type, Accept, Authorization, Bearer',
   });
-    
+
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
   await app.listen(port);
