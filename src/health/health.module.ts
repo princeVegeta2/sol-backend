f027c64f735455b8d59763a2315437a94f8736e9
princeVegeta2 <nellatorbeaufort@gmail.com>
2025-01-24 20:59:30 +0100

    Added the health controller to the module of health 1/24/2025

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { HealthService } from "./health.service";
+import { HealthController } from "./health.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([])],
     providers: [HealthService],
+    controllers: [HealthController],
     exports: [HealthService],
 })
 
