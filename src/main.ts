71b15357a8d10faaec0f15f707c2a208be54b43c
princeVegeta2 <nellatorbeaufort@gmail.com>
2025-01-24 18:19:59 +0100

    EB Configuration Test 7 1/24/2025

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   console.log("Creating nest factory App Module");
   const app = await NestFactory.create(AppModule);
 
+  console.log("Enabling CORS");
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -14,6 +15,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 8080;
+  console.log("Starting up...");
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
 }
